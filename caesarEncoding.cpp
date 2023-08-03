7be61571d678d7eddb8905bb69379f87e0af0b89
dstasche <laila.staschenuk@stud.htwk-leipzig.de>
2023-08-03 14:05:19 +0200

    dev: new: final version, copied to github

---
+++ caesarEncoding.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <fstream>
+
+// encode text
+char rotChar(char c, int n);
+
+// return encoded text
+std::string rotString(std::string &text, int n);
+
+char rotChar(char c, int n)
+{
+    if ('A' <= c && c <= 'Z')
+    {
+        c = c - 'A';
+        c = (c + n) % 26; // % in C++ ((x mod y) + y) mod y: (4 mod 26) + 26) mod 26) = 4
+        c = c + 'A';
+    }
+    else if ('a' <= c && c <= 'z')
+    {
+        c = c - 'a';
+        c = (c + n) % 26;
+        c = c + 'a';
+    }
+    return c;
+}
+
+std::string rotString(std::string &text, int n)
+{
+    std::string out = "";
+    int size = text.size();
+    for (int i = 0; i < text.size(); i++)
+    {
+        char c = text.at(i);
+        c = rotChar(c, n);
+        out.push_back(c);
+    }
+    return (out);
+}
+
+int main()
+{
+    std::string text;
+    std::cout << "Enter your text to be Caesar-encoded (ASCI alphabet, special characters will not be encoded): \n";
+    std::getline(std::cin, text);
+    int n = 0;
+    std::cout << "Encode by how many digits? (ASCI alphabet: modulo 26): \n";
+    std::cin >> n;
+
+    // print encoded text to terminal
+    std::cout << rotString(text, n) << std::endl;
+}
